8a4d1522c9c6aac7f2759194189bb9d7ead487af
yusun-lab <yu.sun@hyperisland.se>
2025-04-08 23:51:59 +0200

    create blog post route, handle searchBar

--- server/routes/main.js
+++ server/routes/main.js
@@ -58,6 +58,66 @@ router.get('/', async (req, res) => {
 
 // });
 
+
+/* 
+  * GET /
+  * Post :id
+*/
+router.get('/post/:id', async (req, res) => {
+  try {
+    
+    let slug = req.params.id
+    
+    const data = await Post.findById({ _id: slug});
+    const locals = {
+      title: data.title,
+      description: 'Simple Blog built with Node.js, Express and MongoDB',
+    };
+    res.render('post', { locals, data });
+  }
+  catch (error) {
+    console.error(error);
+  }
+
+});
+
+
+/* 
+  * POST /
+  * Post - searchTerm
+*/
+
+
+router.post('/search', async (req, res) => {
+  try {
+    const locals = {
+      title: 'Search',
+      description: 'Simple Blog built with Node.js, Express and MongoDB',
+    };
+
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, '');    
+
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChar, 'i') } },
+        { body: { $regex: new RegExp(searchNoSpecialChar, 'i') } }
+      ]
+    });
+    
+    res.render("search", {
+      data,
+      locals
+    });
+  }
+  catch (error) {
+    console.error(error);
+  }
+
+});
+
+
+
 function insertPostData () {
   Post.insertMany([
     {
