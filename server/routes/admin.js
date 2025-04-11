be06304324121eace91b193690b6ef44200abf82
yusun-lab <yu.sun@hyperisland.se>
2025-04-11 22:28:25 +0200

    create route for admin page, handle admin register

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 const User = require("../models/User");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const adminLayout = '../views/layouts/admin';
 
@@ -39,6 +41,35 @@ router.post('/admin', async (req, res) => {
     } else {
       res.send('Wrong username or password!');
     }
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+
+/* 
+  * POST /
+  * Admin - Register
+*/
+router.post('/register', async (req, res) => {
+  try {
+    const { username, password} = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+
+    try {
+      const user = await User.create({ username, password:hashedPassword });
+      res.status(201).json({ message: 'User Created', user });
+    } catch (error) {
+      if(error.code === 11000) {
+        res.status(409).json({ message: 'Username already in use' });
+      }
+      res.status(500).json({ message: 'Internal server error' });
+    }
+
+
+
+    
   }
   catch (error) {
     console.error(error);
