756b976d5a972485588631acbf970da026061978
yusun-lab <yu.sun@hyperisland.se>
2025-04-12 15:14:34 +0200

    create route to dashboard, handle saving cookie

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -6,6 +6,29 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const adminLayout = '../views/layouts/admin';
+const jwtSecret = process.env.JWT_SECRET;
+
+
+/* 
+  * Check Login
+*/
+const authMiddleware = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' }); /* ??? */
+  }
+  
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Unauthorized' });
+  }
+}
+
+
 
 /* 
   * GET /
@@ -36,16 +59,56 @@ router.post('/admin', async (req, res) => {
     
     const { username, password} = req.body;
   
-    if(req.body.username === 'admin'  && req.body.password === 'password') {
-      res.send('You are logged in!');
-    } else {
-      res.send('Wrong username or password!');
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json( {message: 'Invalid credentials'} );
     }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json( {message: 'Invalid credentials'} );
+    }
+
+    const token = jwt.sign({ userId: user._id }, jwtSecret );
+    res.cookie('token', token, { httpOnly: true });
+    res.redirect('/dashboard');
+
+    
+    
   }
   catch (error) {
     console.error(error);
   }
 });
+
+/* 
+  * GET /
+  * Admin - Dashboard
+*/
+router.get('/dashboard', authMiddleware, async (req, res) => {
+
+  res.render('admin/dashboard');
+
+});
+
+// router.post('/admin', async (req, res) => {
+//   try {
+    
+//     const { username, password} = req.body;
+  
+//     if(req.body.username === 'admin'  && req.body.password === 'password') {
+//       res.send('You are logged in!');
+//     } else {
+//       res.send('Wrong username or password!');
+//     }
+    
+//   }
+//   catch (error) {
+//     console.error(error);
+//   }
+// });
 
 /* 
   * POST /
