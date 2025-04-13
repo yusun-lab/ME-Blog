43012f9924667e50582a0671c8cc5affb6910ec6
yusun-lab <yu.sun@hyperisland.se>
2025-04-13 19:01:44 +0200

    handle edit post

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -150,6 +150,52 @@ router.post('/add-post', authMiddleware, async (req, res) => {
   }
 });
 
+
+
+  /* 
+  * GET /
+  * Admin - Edit Post
+*/
+router.get('/edit-post/:id', authMiddleware, async (req, res) => {
+  try {
+
+    const locals = {
+      title: 'Edit Post',
+      description: 'Free NodeJs User Management System',
+    }
+    
+    const data = await Post.findOne({ _id: req.params.id });
+
+    res.render('admin/edit-post',{
+      locals,
+      data,
+      layout: adminLayout,
+    });
+
+  } catch (error) {
+    console.log(error);    
+  }
+});
+
+
+  /* 
+  * PUT /
+  * Admin - Edit Post
+*/
+router.put('/edit-post/:id', authMiddleware, async (req, res) => {
+  try {
+    await Post.findByIdAndUpdate(req.params.id, {
+      title: req.body.title, 
+      body: req.body.body, 
+      updatedAt: Date.now()
+    });
+
+    res.redirect(`/edit-post/${req.params.id}`);
+  } catch (error) {
+    console.log(error);    
+  }
+});
+
 // router.post('/admin', async (req, res) => {
 //   try {
     
